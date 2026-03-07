f403fce4a8848d6fc95b574e2809cd379b652064
Plaiz Design Studio <plaiz@plaizdesign.com>
2026-03-07 02:19:06 +0100

    Deploy Service Worker Kill-Scripts to purge legacy site caches

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+// SW-Kill Script: Forcefully unregisters the Service Worker
+self.addEventListener('install', function (e) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+    self.registration.unregister()
+        .then(function () {
+            return self.clients.matchAll();
+        })
+        .then(function (clients) {
+            clients.forEach(client => client.navigate(client.url));
+        });
+});
